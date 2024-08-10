66588b8a8e0cb7cec6b1c038cf19ffc91c384f33
DESKTOP-AL1M89N <kumarbhattarai666@gmail.com>
2024-08-11 00:14:27 +0545

    import modules and require

--- start/main.js
+++ start/main.js
@@ -1,5 +1,18 @@
-var http = require ('http')
-http.createServer(function(req, res){
-    res.writeHead(200,{'content-type':'text/html'})
-    res.end('<h1>This is my first and second node and another change, more changes,another one</h1>')
-}).listen(8080);
+// var http = require ('http')
+// http.createServer(function(req, res){
+//     res.writeHead(200,{'content-type':'text/html'})
+//     res.end('<h1>This is my first and second node and another change, more changes,another one</h1>')
+// }).listen(8080);
+
+
+// import {a,b,c} from "./module.js"  ----Named export----
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+import obj from "./module.js" //----Defaule export, we can even use any other name rather than 'obj' to import every default export.----
+console.log(obj)
+// ---- These kinds of ES modules work only when we declare '"type":"module",' in package.json just above the debug section----
+// ---- If we want to use require then simply remove that '"type":"module",' and----
+// let a=require("./module2.js")
+// console.log(a)// ---- remember to modify the module2.js file too as required ----
---
+++ start/module.js
@@ -0,0 +1,8 @@
+export const a=1
+export const b=2
+export const c=3
+let obj;
+export default obj = {
+    x: 6,
+    y: 7
+}
