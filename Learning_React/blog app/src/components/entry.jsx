4d49ed7a765c015d6b71704aa9d36d1cce942a04
DESKTOP-AL1M89N <kumarbhattarai666@gmail.com>
2025-09-23 17:03:15 +0545

    passing obj as props

--- Learning_React/blog app/src/components/entry.jsx
+++ Learning_React/blog app/src/components/entry.jsx
@@ -1,11 +1,13 @@
-export function Entry(props) {
+
+
+export default function Entry(props) {
     return (
         <article className="journal-entry">
             <div className="main-image-container">
                 <img 
                     className="main-image"
-                    src={props.img.src}
-                    alt={props.img.alt}
+                    src={props.entry.img.src} 
+                    alt={props.entry.img.alt}
                 />
             </div>
             <div className="info-container">
@@ -14,11 +16,11 @@ export function Entry(props) {
                     src="../images/marker.png" 
                     alt="map marker icon"
                 />
-                <span className="country">{props.country}</span>
-                <a href={props.googleMapsLink}>view on google maps</a>
-                <h2 className="entry-title">{props.title}</h2>
-                <p className="trip-dates">{props.dates}</p>
-                <p className="entry-text">{props.text}</p>
+                <span className="country">{props.entry.country}</span>
+                <a href={props.entrygoogleMapsLink} target="_blank">View on Google Maps</a>
+                <h2 className="entry-title">{props.entry.title}</h2>
+                <p className="trip-dates">{props.entry.dates}</p>
+                <p className="entry-text">{props.entry.text}</p>
             </div>
             
         </article>
